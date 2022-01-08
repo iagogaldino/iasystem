e6625e57b63e9dda55a7eaa59aff02aed7f04d1e
Iago Delsuc Galdino Santos <iago_galdino@hotmail.com>
2022-01-08 10:33:02 -0300

    das

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
 
 
-@app.route("/")
-def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return f"The configured secret key is {secret_key}."
+@app.route("/teste", methods=["GET"])
+def helloWord():
+    return "Hello"
+
+def main():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
+
+
+ 
